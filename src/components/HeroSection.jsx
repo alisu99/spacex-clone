d44e452e10c3e9b31600598088038744c3aa6bc0
alisu99 <alissonsts910@gmail.com>
2025-06-28 00:37:42 -0300

    Atualização transições

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,21 +3,24 @@ export default function HeroSection() {
     <>
       <div className="relative h-screen w-full herosection custom-padding flex justify-center">
         <div className="relative z-10 flex flex-col justify-end h-full bg-opacity-50 px-5 pb-[5rem] w-full custom-mw">
-          <p className="text-lg max-w-xl text-white/80">
+          <p className="text-lg max-w-xl text-white/80 animate-fade-in-up delay-[0ms]">
             RECENT LAUNCH
           </p>
-          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem]">AX-4 MISSION</h2>
-          <a href="" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block">
+          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem] animate-fade-in-up delay-[150ms]">
+            AX-4 MISSION
+          </h2>
+          <a href="#" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block animate-fade-in-up delay-[300ms]">
             <small className="tracking-tightest">REWATCH</small>
           </a>
-
         </div>
       </div>
 
       <div className="relative h-screen w-full background2 custom-padding flex justify-center">
-        <div className="relative z-10 flex flex-col justify-end h-full bg-opacity-50 px-5 pb-[5rem] w-full custom-mw">
-          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem]">THE ROAD TO MAKING LIFE MULTIPLANETARY </h2>
-          <a href="" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block">
+        <div className="relative z-10 flex flex-col justify-end h-full bg-opacity-50 px-5 pb-[5rem] w-full custom-mw animate-fade-in-up delay-[0ms]">
+          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem] animate-fade-in-up delay-[150ms]">
+            THE ROAD TO MAKING LIFE MULTIPLANETARY
+          </h2>
+          <a href="#" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block animate-fade-in-up delay-[300ms]">
             <small className="tracking-tightest">LEARN MORE</small>
           </a>
         </div>
@@ -25,23 +28,27 @@ export default function HeroSection() {
 
       <div className="relative h-screen w-full background3 custom-padding flex justify-center">
         <div className="relative z-10 flex flex-col justify-end h-full bg-opacity-50 px-5 pb-[5rem] w-full custom-mw">
-          <p className="text-lg max-w-xl text-white/80">
+          <p className="text-lg max-w-xl text-white/80 animate-fade-in-up delay-[0ms]">
             UPCOMING LAUNCH
           </p>
-          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem]">STARLINK MISSION</h2>
-          <a href="" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block">
-            <smal className="tracking-tightest">WATCH</smal>
+          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem] animate-fade-in-up delay-[150ms]">
+            STARLINK MISSION
+          </h2>
+          <a href="#" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block animate-fade-in-up delay-[300ms]">
+            <small className="tracking-tightest">WATCH</small>
           </a>
         </div>
       </div>
 
       <div className="relative h-screen w-full background4 custom-padding flex justify-center">
         <div className="relative z-10 flex flex-col justify-end h-full bg-opacity-50 px-5 pb-[5rem] w-full custom-mw">
-          <p className="text-lg max-w-xl text-white/80">
+          <p className="text-lg max-w-xl text-white/80 animate-fade-in-up delay-[0ms]">
             RECENT LAUNCH
           </p>
-          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem]">STARSHIP'S NINTH FLIGHT TEST</h2>
-          <a href="" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block">
+          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem] animate-fade-in-up delay-[150ms]">
+            STARSHIP'S NINTH FLIGHT TEST
+          </h2>
+          <a href="#" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block animate-fade-in-up delay-[300ms]">
             <small className="tracking-tightest">REWATCH</small>
           </a>
         </div>
@@ -49,12 +56,14 @@ export default function HeroSection() {
 
       <div className="relative h-screen w-full background5 custom-padding flex justify-center">
         <div className="relative z-10 flex flex-col justify-end h-full bg-opacity-50 px-5 pb-[5rem] w-full custom-mw">
-          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem]">ADVANCING HUMAN SPACEFLIGHT</h2>
-          <a href="" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block">
+          <h2 className="font-bold text-white tracking-tightest leading-[2.25rem] animate-fade-in-up delay-[0ms]">
+            ADVANCING HUMAN SPACEFLIGHT
+          </h2>
+          <a href="#" className="custom-btn text-center font-black min-w-[130px] px-8 py-3 mt-[15px] border-2 border-white w-max inline-block animate-fade-in-up delay-[150ms]">
             <small className="tracking-tightest">LEARN MORE</small>
           </a>
         </div>
       </div>
     </>
-  )
+  );
 }
